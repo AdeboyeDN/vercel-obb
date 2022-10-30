8f30e497f3ab5a30543418608d0b461f8a7d3880
Daniel <adeboyed93@gmail.com>
2022-10-30 12:01:02 -0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_KEY)
+  console.log(process.env.NEXT_PUBLIC_NEW)
   return (
     <div className={styles.container}>
       <Head>
