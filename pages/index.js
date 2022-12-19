072207d6fb67e07a589309b1d8e894b80727b656
Daniel Adeboye <adeboyed93@gmail.com>
2022-12-19 04:59:36 -0800

    Minor update

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">PipeOps!</a>
+          Hello <a href="https://nextjs.org">PipeOps!</a>
         </h1>
 
         <p className={styles.description}>
